c3a4cabb7fdcd7fa8cdfa4e6640c08fe55b0f3f6
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-06-26 21:56:57 +0500

    OceanVersion2

---
+++ OceanVersion2/OceanVersion2/BL/AngleBL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OceanNavigation.BL
+{
+    class ANGLE
+    {
+        public ANGLE(ANGLE s)
+        {
+            this.degree = s.degree;
+            this.minute = s.minute;
+            this.direction = s.direction;
+        }
+        public ANGLE (int degree , float min ,char direction)
+        {
+            this.degree = degree;
+            this.minute = min;
+            this.direction = direction;
+        }
+        private int degree;
+        private float minute;
+        private char direction;
+
+       public int getDegree()
+        {
+            return degree;
+        }
+        public float getMinute()
+        {
+            return minute;
+        }
+        public char getDirection()
+        {
+            return direction;
+        }
+
+    }
+}
---
+++ OceanVersion2/OceanVersion2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OceanNavigation.BL;
+using OceanVersion2.Menu;
+using OceanVersion2.UI;
+using OceanVersion2.DL;
+namespace OceanNavigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShipDL.loadShip();
+           
+            int op = 0;
+            while (true)
+            {
+                Console.Clear();
+                op =MenuUI.DriverMenu();
+                if (op == 1)
+                {
+                    SHIP s =ShipUI.addShip();
+                    ShipDL.addIntoList(s);
+                    ShipDL.addIntoFile(s);
+
+                }
+                else if (op == 2)
+                {
+                   ShipUI.viewShipPosition(ShipDL.shipList);
+                }
+                else if (op == 3)
+                {
+                   ShipUI.viewShipSerialNumber(ShipDL.shipList);
+                }
+                else if (op == 4)
+                {
+                   ShipUI.changeShipPosition(ShipDL.shipList);
+                }
+                else if (op == 5)
+                {
+                    break;
+                }
+            }
+
+        }
+        
+
+       
+       
+      
+       
+    }
+}
